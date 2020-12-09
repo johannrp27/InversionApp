a6fac8792519136bc233a0c27b2def6721504975
Johann <johannrip@gmail.com>
2020-12-09 14:10:35 -0500

    Basic draft - U

--- src/app/views/portafolio/portafolio.component.ts
+++ src/app/views/portafolio/portafolio.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PortafolioComponent implements OnInit {
   @Input() type: string;
-  
+
+  company: any;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getPortfolio();
+  }
+
+  getPortfolio(){
+    this.company = {
+      name: 'FIC Old Mutual Efectivo',
+      icon: '../../../assets/icons/partner.svg',
+      balance: 240000,
+      percent: 15,
+      model: [
+        'Renta Fija',
+        'Depósito a la fija'
+      ]
+    };
   }
 
 }
