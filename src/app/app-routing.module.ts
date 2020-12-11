dbc3d13eb31f9a4cbea3cebfc2cf58f028c18d62
Johann <johannrip@gmail.com>
2020-12-10 19:29:47 -0500

    With routes / No svg Color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-
-
+import { OtherComponent } from './views/other/other.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: HomeComponent}
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'other', component: OtherComponent}
 ];
 
 @NgModule({
